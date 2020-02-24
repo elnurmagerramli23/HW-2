b546660a7770bceac2abc637371c4708d5c54a28
elnurmagerramli23 <elnurmagerramli@gmail.com>
2020-02-24 15:31:07 +0400

    lastest

--- index.js
+++ index.js
@@ -1,56 +1,124 @@
-// // 1.3 Triangle
+//1 Square
 
-function triangles(number) {
-    let triangle = "";
-    
-    for ( triangle = ""; triangle.length <= 10; triangle += "*" ){
-        console.log(triangle);
-    }
-};
-
-console.log(triangles(10));
-
-// //1.1 Square
-
-
-function squares( width, height ) {
+function getSquares(size) {
     let square = "";
 
-    for( let i = 0; i < height; i++ ){
-        for( let j = 0; j < width; j++ ){
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
             square += "* ";
-            if ( j == width - 1){
+            if (j === size - 1) {
                 square += '\n';
             }
         }
-    }return square
+    }
+
+    return square;
 };
 
-console.log(squares(5, 5));
+console.log( getSquares(10) );
 
 
+//2
 
-//1.3.2
+function getEmptySquare(size) {
+    let square = "";
 
-
-function secondSquares(en, uzunlug) {
-    let secondSquare = "";
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            if (i === 0 || i === size - 1 || j === 0 || j === size - 1) {
+                square += " *";
+            } else {
+                square += "  ";
+            }
+        } 
+        square += "\n";
+    }
     
-    for ( let i = 0; i < uzunlug; i++ ){
-        for ( let j = 0; j < en; j++ ){
-            if ( i == 0 || i == uzunlug - 1 )
-                secondSquare += "*";
-            else {
-                if ( j == 0 || j == en - 1)
-                    secondSquare += "*";
-                else
-                    secondSquare += " ";
-                    
-            }if ( j == en - 1){
-                secondSquare+= "\n";
+    return square;
+};
+
+console.log( getEmptySquare(10) );
+
+
+// 3 Triangle
+function getTriangle(size) {
+    let triangle = "";
+    
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            if (i + j < size)
+                triangle += "*";
+        }
+        triangle += "\n";
+    }
+    
+    return triangle;
+};
+
+console.log( getTriangle(10) );
+//4
+
+function getEmptyTriangle(size) {
+    let triangle = "";
+    
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            if (i === 0 || (i === size - 1 - j) || j === 0) {
+                triangle += "*";
+            } else {
+                triangle += " ";
             }
         }
-    }return secondSquare
-}
+        
+        triangle += "\n";
+    }
 
-console.log(secondSquares(5, 5))
+    return triangle;
+};
+
+console.log( getEmptyTriangle(10) );
+//5
+
+function getHourglass(size) {
+    let hourglass = "";
+
+    for ( let i = 0; i < size; i++ ) {
+        for ( let j = 0; j < size; j++ ) {
+            if ((i === size - 1 - j) || i === j || (i < j && i + j < size ) || ( j < i && i + j >= size) ) {
+                hourglass += "*";
+            } else { 
+                hourglass += " ";
+        } 
+
+        }
+        hourglass += "\n";
+    }
+    
+    return hourglass;
+};
+
+console.log( getHourglass(10));
+
+
+//6
+
+
+function getEmptyHourglass(size) {
+    let hourglass = "";
+
+    for ( let i = 0; i < size; i++ ) {
+        for ( let j = 0; j < size; j++ ) {
+            if (i === 0 || (i === size - 1 - j) || i === size - 1 || i === j) {
+                
+            hourglass += "*"
+            } else { 
+                hourglass += " ";
+            } 
+        }
+        hourglass += "\n";
+    }
+    
+    return hourglass;
+};
+
+console.log( getEmptyHourglass(10));
